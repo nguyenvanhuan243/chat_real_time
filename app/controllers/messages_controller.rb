9f11959f09742dfb06a674303d0046f1b3cf3471
Huan Nguyen <nguyenvanhuan243@gmail.com>
2023-05-06 18:56:07 +0700

    produce message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,23 @@
 class MessagesController < ApplicationController
 
   def create
-    message = Message.new(message_params)
-    message.user = current_user
-    if message.save
-      ActionCable.server.broadcast 'messages',
-        message: message.content,
-        user: message.user.username
-      head :ok
-    end
+    message = Message.new(message_params.merge(user_id: current_user.id))
+    produce_message(message) if message.save
   end
 
   private
 
-    def message_params
-      params.require(:message).permit(:content, :chatroom_id)
-    end
+  def message_params
+    params.require(:message).permit(:content, :chatroom_id)
+  end
+
+  def produce_message(message)
+    ActionCable.server.broadcast 'channel_messages',
+      message: message.content,
+      user: message.user.username
+    head :ok
+    puts "################## produce_message DONE!"
+    puts "################## produce_message DONE!"
+    puts "################## produce_message DONE!"
+  end
 end
